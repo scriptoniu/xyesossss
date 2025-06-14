fab686635ef6574e043f9862fe2dcf6ba6bd3279
scriptoniu <pidorasnegr45@mail.ru>
2025-06-15 01:51:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+api_id = 25365650
+api_hash = '2ba31a5afb1366e1cdd233dbcdec49a7'
+
+channel = 
+
+avto_vivod = True
+avto_vivod_tag = mand3laeff
+
+avto_otpiska = False
+
+anti_captcha = True
+
+ocr_api_key = 'K85698966788957'
